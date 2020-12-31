55ef158888974522ef066c7d701b97507ef068f8
Rodrigo Espinoza <rodrigo.espinoza@me.com>
2020-12-31 00:01:02 -0800

    add missing create cookie

--- public/script.js
+++ public/script.js
@@ -192,3 +192,13 @@ function readCookie(name) {
 function eraseCookie(name) {
 	createCookie(name, '', -1);
 }
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
